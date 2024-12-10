d7227c272c07cb8c176db970d978842ee6de8eb1
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-12-10 22:19:45 +0900

    Enhance PostalCode model with additional fields and CSV parsing logic

--- lib/models/postal_code.dart
+++ lib/models/postal_code.dart
@@ -1,4 +1,5 @@
 class PostalCode {
+  final String id;
   final String jisCode;
   final String oldZipCode;
   final String zipCode;
@@ -8,8 +9,16 @@ class PostalCode {
   final String prefecture;
   final String city;
   final String town;
+  final bool hasMultipleZipCodes;
+  final bool hasNumberedEveryKoaza;
+  final bool hasChome;
+  final bool hasMultipleAreas;
+  final String updateStatus;
+  final String updateReason;
+  final DateTime updatedAt;
 
   PostalCode({
+    required this.id,
     required this.jisCode,
     required this.oldZipCode,
     required this.zipCode,
@@ -19,10 +28,18 @@ class PostalCode {
     required this.prefecture,
     required this.city,
     required this.town,
+    required this.hasMultipleZipCodes,
+    required this.hasNumberedEveryKoaza,
+    required this.hasChome,
+    required this.hasMultipleAreas,
+    required this.updateStatus,
+    required this.updateReason,
+    required this.updatedAt,
   });
 
   factory PostalCode.fromCsv(List<String> row) {
     return PostalCode(
+      id: '${row[2]}_${row[7]}_${row[8]}',
       jisCode: row[0],
       oldZipCode: row[1],
       zipCode: row[2],
@@ -32,6 +49,13 @@ class PostalCode {
       prefecture: row[6],
       city: row[7],
       town: row[8],
+      hasMultipleZipCodes: row[9] == '1',
+      hasNumberedEveryKoaza: row[10] == '1',
+      hasChome: row[11] == '1',
+      hasMultipleAreas: row[12] == '1',
+      updateStatus: row[13],
+      updateReason: row[14],
+      updatedAt: DateTime.now(),
     );
   }
 }
